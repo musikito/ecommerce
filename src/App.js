18c45bdaf86b09eef161658bce75455c6695a304
jose marte <tumusicalatina@gmail.com>
2019-06-07 15:29:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import {Switch, Route} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart/";
+import Default from "./components/Default";
+import Modal from './components/Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route exact path="/details" component={Details} />
+          <Route exact path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
